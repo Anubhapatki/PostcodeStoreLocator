66f8994595a4cb62d012f903f8373fd79691726c
anubha-hivehome <anubha.vijay@hivehome.com>
2019-04-19 01:54:49 +0100

    template rendering

--- stores/models.py
+++ stores/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-class PostCode(models.Model):
-    postcode = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
+class Stores(models.Model):
+    postcode = models.CharField(max_length=7)
+    location = models.CharField(max_length=50)
+    latitude = models.DecimalField(max_digits=8, decimal_places=5, blank=False, null=True)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5, blank=False, null=True)
+
 
--- stores/views.py
+++ stores/views.py
@@ -1,56 +1,46 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from stores.models import Stores
 import requests
 
 import json
 from operator import itemgetter
 
 def index(request):
+
+    Stores.objects.all().delete()
     with open('stores.json') as f:
         data = json.load(f)
+    for d in data:
+        store = Stores(
+            location = d['name'],
+            postcode = d['postcode']
+            )
+        store.save()
+
     data.sort(key=itemgetter('name'))
     data_log_lat = getPostCodesLatitudeLogitude(data)
 
-    postcodes_log_lat =list()
-
     for data in data_log_lat["result"]:
-
         try:
-            print("data{}".format(data))
-            postcodes_log_lat_dict = dict()
-
-
-
-            postcodes_log_lat_dict["postcodes"] = data["query"]
-            postcodes_log_lat_dict["location"] = data["result"]["ced"]
-
-            postcodes_log_lat_dict["longitude"] = data["result"]["longitude"]
-            postcodes_log_lat_dict["latitude"] = data["result"]["latitude"]
-            postcodes_log_lat.append(postcodes_log_lat_dict)
+            store = Stores.objects.filter(postcode = data["query"]).\
+                update(longitude = data["result"]["longitude"],
+                       latitude=data["result"]["latitude"])
+            store.save()
         except:
             pass
 
-    return JsonResponse(postcodes_log_lat, safe=False)
-    #return render(request, '/Users/Anubha.Vijay/storelocator/storelocator/templates/stores/index.html', data)
-    #return HttpResponse(data)
+    context = {'Stores': Stores.objects.all().order_by('location')}
 
-def postcodes(request):
-    with open('../stores.json') as f:
-        data = json.load(f)
-    data.sort(key=itemgetter('name'))
-    return render(request, 'index.html', data)
-    #return JsonResponse(data, safe=False)
+    return render(request, 'stores/index.html', context)
+
 
 
 def getPostCodesLatitudeLogitude(data):
     postcode_data = { "postcodes": [ d['postcode'] for d in data ]}
-   # print ("postcode_data:{}".format(json.dumps(postcode_data)))
     headers = {'Content-Type': 'application/json'}
-
     resp = requests.post('https://api.postcodes.io/postcodes/', data=json.dumps(postcode_data), headers=headers)
-   # print(resp.text)
     resp.json()
-   # print(resp)
     return json.loads(resp.text)
 
 
